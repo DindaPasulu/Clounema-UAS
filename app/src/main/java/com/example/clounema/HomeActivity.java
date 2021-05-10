053fa6566738a5955d78fca14261f3e20a079f1b
DindaPasulu <dinda.prilliagrandi@student.umn.ac.id>
2021-05-10 15:23:29 +0700

    revisi home layout

--- app/src/main/java/com/example/clounema/HomeActivity.java
+++ app/src/main/java/com/example/clounema/HomeActivity.java
@@ -2,25 +2,20 @@ package com.example.clounema;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.WindowManager;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
-    //Initialize bottom navbar
-    BottomNavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         //assign variable
-        navigationView = findViewById(R.id.bottom_navbar);
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navbar);
         //set home selected
         navigationView.setSelectedItemId(R.id.nav_home);
 
@@ -32,13 +27,11 @@ public class HomeActivity extends AppCompatActivity {
                     case R.id.nav_home:
                         return true;
                     case R.id.nav_ticket:
-                        startActivity(new Intent(getApplicationContext(),
-                                OrderDetails.class));
+                        startActivity(new Intent(getApplicationContext(), OrderDetails.class));
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.nav_profile:
-                        startActivity(new Intent(getApplicationContext(),
-                                ProfileActivity.class));
+                        startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
                         overridePendingTransition(0,0);
                         return true;
                 }
